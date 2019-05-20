d9301e32fb1684aa0fb34b132376bcb61d2d1bdc
Fernando Brito <fernando.brito@naturalcycles.com>
2019-05-20 10:50:20 +0200

    feat: add dateScrubber and undefinedScrubber

--- src/scrubbers.ts
+++ src/scrubbers.ts
@@ -2,6 +2,7 @@ import { ScrubberFn, ScrubbersImpl } from './scrubber.model'
 
 export const ANONYMIZED_EMAIL = 'anonymized@nc.com'
 
+// export type dateScrubberFn = (value: string, params: { excludeDay?: boolean }) => string
 // export type staticScrubberFn = (value: string, params: { replacement: string }) => string
 export type emailScrubberFn = () => string
 
@@ -9,7 +10,15 @@ export const staticScrubber: ScrubberFn = (value, params = {}) => params.replace
 
 export const emailScrubber: emailScrubberFn = () => ANONYMIZED_EMAIL
 
+export const undefinedScrubber: ScrubberFn = () => undefined
+
+export const dateScrubber: ScrubberFn = (value, params = {}) => {
+  return value.substr(0, 7) + '-01'
+}
+
 export const defaultScrubbers: ScrubbersImpl = {
   staticScrubber,
   emailScrubber,
+  dateScrubber,
+  undefinedScrubber,
 }
